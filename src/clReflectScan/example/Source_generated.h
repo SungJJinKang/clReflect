b13e4121830b576e18a62a9f49b00fcd85edad96
sungJinKangdev <hour30000@gmail.com>
2021-11-12 21:34:13 +0900

    add example code files.

---
+++ src/clReflectScan/example/Source_generated.h
@@ -0,0 +1,398 @@
+#pragma once
+
+// Utility Header File ( Don't Edit this )
+SourceFilePath : C:\Users\kmsjk\source\repos\Project7\Source.cpp
+
+
+#ifdef C:\Users\kmsjk\source\repos\Project7\Source_generated.h
+#error "C:\Users\kmsjk\source\repos\Project7\Source_generated.h already included, missing '#pragma once' in C:\Users\kmsjk\source\repos\Project7\Source.h"
+#endif
+#ifdef GENERATE_BODY_TestClass2
+#error "GENERATE_BODY_TestClass2 already included...."
+#endif
+
+
+#undef CURRENT_TYPE_ALIAS_TestClass2
+#define CURRENT_TYPE_ALIAS_TestClass2 \
+typedef TestClass2 Current
+
+
+#undef TYPE_FULLNAME_HASH_VALUE_NAME_STRING_TestClass2
+#define TYPE_FULLNAME_HASH_VALUE_NAME_STRING_TestClass2 \
+inline static const unsigned long int TYPE_FULL_NAME_HASH_VALUE = -1726398595; \
+inline static const char* const TYPE_FULL_NAME = TestClass2; \
+inline static const char* const TYPE_SHORT_NAME = TestClass2; 
+
+
+#undef GENERATE_BODY_TestClass2
+#define GENERATE_BODY_TestClass2 \
+CURRENT_TYPE_ALIAS_TestClass2 \
+TYPE_FULLNAME_HASH_VALUE_NAME_STRING_TestClass2 \
+
+
+#define GENERATE_BODY_TestClass2 GENERATE_BODY_TestClass2
+
+
+-------------------------------------------
+
+
+
+
+-------------------------------------------
+
+
+
+
+-------------------------------------------
+
+
+
+
+-------------------------------------------
+
+
+
+
+-------------------------------------------
+
+
+
+
+-------------------------------------------
+
+
+
+
+-------------------------------------------
+
+
+
+
+-------------------------------------------
+
+
+
+
+-------------------------------------------
+
+
+
+
+-------------------------------------------
+
+
+
+
+-------------------------------------------
+
+
+
+
+-------------------------------------------
+
+
+#ifdef GENERATE_BODY_TestStruct
+#error "GENERATE_BODY_TestStruct already included...."
+#endif
+
+
+#undef CURRENT_TYPE_ALIAS_TestStruct
+#define CURRENT_TYPE_ALIAS_TestStruct \
+typedef TestStruct Current
+
+
+#undef TYPE_FULLNAME_HASH_VALUE_NAME_STRING_TestStruct
+#define TYPE_FULLNAME_HASH_VALUE_NAME_STRING_TestStruct \
+inline static const unsigned long int TYPE_FULL_NAME_HASH_VALUE = 248002123; \
+inline static const char* const TYPE_FULL_NAME = TestStruct; \
+inline static const char* const TYPE_SHORT_NAME = TestStruct; 
+
+
+#undef GENERATE_BODY_TestStruct
+#define GENERATE_BODY_TestStruct \
+CURRENT_TYPE_ALIAS_TestStruct \
+TYPE_FULLNAME_HASH_VALUE_NAME_STRING_TestStruct \
+
+
+#define GENERATE_BODY_TestStruct GENERATE_BODY_TestStruct
+
+
+-------------------------------------------
+
+
+
+
+-------------------------------------------
+
+
+#ifdef GENERATE_BODY_test_base_chain__B
+#error "GENERATE_BODY_test_base_chain__B already included...."
+#endif
+
+#undef INHERITANCE_INFORMATION_test_base_chain__B
+#define INHERITANCE_INFORMATION_test_base_chain__B \
+private: inline static const unsigned long int BASE_CHAIN_LIST[] { 2207817649 }; \
+
+
+#undef CURRENT_TYPE_ALIAS_test_base_chain__B
+#define CURRENT_TYPE_ALIAS_test_base_chain__B \
+typedef test_base_chain::B Current
+
+
+#undef TYPE_FULLNAME_HASH_VALUE_NAME_STRING_test_base_chain__B
+#define TYPE_FULLNAME_HASH_VALUE_NAME_STRING_test_base_chain__B \
+inline static const unsigned long int TYPE_FULL_NAME_HASH_VALUE = -2087149647; \
+inline static const char* const TYPE_FULL_NAME = test_base_chain::B; \
+inline static const char* const TYPE_SHORT_NAME = B; 
+
+
+#undef GENERATE_BODY_test_base_chain__B
+#define GENERATE_BODY_test_base_chain__B \
+INHERITANCE_INFORMATION_test_base_chain__B \
+CURRENT_TYPE_ALIAS_test_base_chain__B \
+TYPE_FULLNAME_HASH_VALUE_NAME_STRING_test_base_chain__B \
+
+
+#define GENERATE_BODY_B GENERATE_BODY_test_base_chain__B
+
+
+-------------------------------------------
+
+
+#ifdef GENERATE_BODY_test_base_chain__TEST1
+#error "GENERATE_BODY_test_base_chain__TEST1 already included...."
+#endif
+
+
+#undef CURRENT_TYPE_ALIAS_test_base_chain__TEST1
+#define CURRENT_TYPE_ALIAS_test_base_chain__TEST1 \
+typedef test_base_chain::TEST1 Current
+
+
+#undef TYPE_FULLNAME_HASH_VALUE_NAME_STRING_test_base_chain__TEST1
+#define TYPE_FULLNAME_HASH_VALUE_NAME_STRING_test_base_chain__TEST1 \
+inline static const unsigned long int TYPE_FULL_NAME_HASH_VALUE = 790946185; \
+inline static const char* const TYPE_FULL_NAME = test_base_chain::TEST1; \
+inline static const char* const TYPE_SHORT_NAME = TEST1; 
+
+
+#undef GENERATE_BODY_test_base_chain__TEST1
+#define GENERATE_BODY_test_base_chain__TEST1 \
+CURRENT_TYPE_ALIAS_test_base_chain__TEST1 \
+TYPE_FULLNAME_HASH_VALUE_NAME_STRING_test_base_chain__TEST1 \
+
+
+#define GENERATE_BODY_TEST1 GENERATE_BODY_test_base_chain__TEST1
+
+
+-------------------------------------------
+
+
+#ifdef GENERATE_BODY_test_base_chain__TEST2
+#error "GENERATE_BODY_test_base_chain__TEST2 already included...."
+#endif
+
+
+#undef CURRENT_TYPE_ALIAS_test_base_chain__TEST2
+#define CURRENT_TYPE_ALIAS_test_base_chain__TEST2 \
+typedef test_base_chain::TEST2 Current
+
+
+#undef TYPE_FULLNAME_HASH_VALUE_NAME_STRING_test_base_chain__TEST2
+#define TYPE_FULLNAME_HASH_VALUE_NAME_STRING_test_base_chain__TEST2 \
+inline static const unsigned long int TYPE_FULL_NAME_HASH_VALUE = -1425998550; \
+inline static const char* const TYPE_FULL_NAME = test_base_chain::TEST2; \
+inline static const char* const TYPE_SHORT_NAME = TEST2; 
+
+
+#undef GENERATE_BODY_test_base_chain__TEST2
+#define GENERATE_BODY_test_base_chain__TEST2 \
+CURRENT_TYPE_ALIAS_test_base_chain__TEST2 \
+TYPE_FULLNAME_HASH_VALUE_NAME_STRING_test_base_chain__TEST2 \
+
+
+#define GENERATE_BODY_TEST2 GENERATE_BODY_test_base_chain__TEST2
+
+
+-------------------------------------------
+
+
+#ifdef GENERATE_BODY_test_base_chain__TEST3
+#error "GENERATE_BODY_test_base_chain__TEST3 already included...."
+#endif
+
+
+#undef CURRENT_TYPE_ALIAS_test_base_chain__TEST3
+#define CURRENT_TYPE_ALIAS_test_base_chain__TEST3 \
+typedef test_base_chain::TEST3 Current
+
+
+#undef TYPE_FULLNAME_HASH_VALUE_NAME_STRING_test_base_chain__TEST3
+#define TYPE_FULLNAME_HASH_VALUE_NAME_STRING_test_base_chain__TEST3 \
+inline static const unsigned long int TYPE_FULL_NAME_HASH_VALUE = -1064949612; \
+inline static const char* const TYPE_FULL_NAME = test_base_chain::TEST3; \
+inline static const char* const TYPE_SHORT_NAME = TEST3; 
+
+
+#undef GENERATE_BODY_test_base_chain__TEST3
+#define GENERATE_BODY_test_base_chain__TEST3 \
+CURRENT_TYPE_ALIAS_test_base_chain__TEST3 \
+TYPE_FULLNAME_HASH_VALUE_NAME_STRING_test_base_chain__TEST3 \
+
+
+#define GENERATE_BODY_TEST3 GENERATE_BODY_test_base_chain__TEST3
+
+
+-------------------------------------------
+
+
+#ifdef GENERATE_BODY_test_base_chain__C
+#error "GENERATE_BODY_test_base_chain__C already included...."
+#endif
+
+#undef INHERITANCE_INFORMATION_test_base_chain__C
+#define INHERITANCE_INFORMATION_test_base_chain__C \
+private: inline static const unsigned long int BASE_CHAIN_LIST[] { 1074097276, 2207817649 }; \
+typedef test_base_chain::C Base
+
+
+#undef CURRENT_TYPE_ALIAS_test_base_chain__C
+#define CURRENT_TYPE_ALIAS_test_base_chain__C \
+typedef test_base_chain::C Current
+
+
+#undef TYPE_FULLNAME_HASH_VALUE_NAME_STRING_test_base_chain__C
+#define TYPE_FULLNAME_HASH_VALUE_NAME_STRING_test_base_chain__C \
+inline static const unsigned long int TYPE_FULL_NAME_HASH_VALUE = 1074097276; \
+inline static const char* const TYPE_FULL_NAME = test_base_chain::C; \
+inline static const char* const TYPE_SHORT_NAME = C; 
+
+
+#undef GENERATE_BODY_test_base_chain__C
+#define GENERATE_BODY_test_base_chain__C \
+INHERITANCE_INFORMATION_test_base_chain__C \
+CURRENT_TYPE_ALIAS_test_base_chain__C \
+TYPE_FULLNAME_HASH_VALUE_NAME_STRING_test_base_chain__C \
+
+
+#define GENERATE_BODY_C GENERATE_BODY_test_base_chain__C
+
+
+-------------------------------------------
+
+
+
+
+-------------------------------------------
+
+
+
+
+-------------------------------------------
+
+
+#ifdef GENERATE_BODY_test_base_chain__D
+#error "GENERATE_BODY_test_base_chain__D already included...."
+#endif
+
+#undef INHERITANCE_INFORMATION_test_base_chain__D
+#define INHERITANCE_INFORMATION_test_base_chain__D \
+private: inline static const unsigned long int BASE_CHAIN_LIST[] { 3583775027, 1074097276, 2207817649 }; \
+typedef test_base_chain::C Base
+
+
+#undef CURRENT_TYPE_ALIAS_test_base_chain__D
+#define CURRENT_TYPE_ALIAS_test_base_chain__D \
+typedef test_base_chain::D Current
+
+
+#undef TYPE_FULLNAME_HASH_VALUE_NAME_STRING_test_base_chain__D
+#define TYPE_FULLNAME_HASH_VALUE_NAME_STRING_test_base_chain__D \
+inline static const unsigned long int TYPE_FULL_NAME_HASH_VALUE = -711192269; \
+inline static const char* const TYPE_FULL_NAME = test_base_chain::D; \
+inline static const char* const TYPE_SHORT_NAME = D; 
+
+
+#undef GENERATE_BODY_test_base_chain__D
+#define GENERATE_BODY_test_base_chain__D \
+INHERITANCE_INFORMATION_test_base_chain__D \
+CURRENT_TYPE_ALIAS_test_base_chain__D \
+TYPE_FULLNAME_HASH_VALUE_NAME_STRING_test_base_chain__D \
+
+
+#define GENERATE_BODY_D GENERATE_BODY_test_base_chain__D
+
+
+-------------------------------------------
+
+
+#ifdef GENERATE_BODY_test_base_chain__F
+#error "GENERATE_BODY_test_base_chain__F already included...."
+#endif
+
+#undef INHERITANCE_INFORMATION_test_base_chain__F
+#define INHERITANCE_INFORMATION_test_base_chain__F \
+private: inline static const unsigned long int BASE_CHAIN_LIST[] { 3931517251, 3583775027, 1074097276, 2207817649 }; \
+typedef test_base_chain::C Base
+
+
+#undef CURRENT_TYPE_ALIAS_test_base_chain__F
+#define CURRENT_TYPE_ALIAS_test_base_chain__F \
+typedef test_base_chain::F Current
+
+
+#undef TYPE_FULLNAME_HASH_VALUE_NAME_STRING_test_base_chain__F
+#define TYPE_FULLNAME_HASH_VALUE_NAME_STRING_test_base_chain__F \
+inline static const unsigned long int TYPE_FULL_NAME_HASH_VALUE = -363450045; \
+inline static const char* const TYPE_FULL_NAME = test_base_chain::F; \
+inline static const char* const TYPE_SHORT_NAME = F; 
+
+
+#undef GENERATE_BODY_test_base_chain__F
+#define GENERATE_BODY_test_base_chain__F \
+INHERITANCE_INFORMATION_test_base_chain__F \
+CURRENT_TYPE_ALIAS_test_base_chain__F \
+TYPE_FULLNAME_HASH_VALUE_NAME_STRING_test_base_chain__F \
+
+
+#define GENERATE_BODY_F GENERATE_BODY_test_base_chain__F
+
+
+-------------------------------------------
+
+
+#ifdef GENERATE_BODY_test_base_chain__G
+#error "GENERATE_BODY_test_base_chain__G already included...."
+#endif
+
+#undef INHERITANCE_INFORMATION_test_base_chain__G
+#define INHERITANCE_INFORMATION_test_base_chain__G \
+private: inline static const unsigned long int BASE_CHAIN_LIST[] { 244226146, 3931517251, 3583775027, 1074097276, 2207817649 }; \
+typedef test_base_chain::C Base
+
+
+#undef CURRENT_TYPE_ALIAS_test_base_chain__G
+#define CURRENT_TYPE_ALIAS_test_base_chain__G \
+typedef test_base_chain::G Current
+
+
+#undef TYPE_FULLNAME_HASH_VALUE_NAME_STRING_test_base_chain__G
+#define TYPE_FULLNAME_HASH_VALUE_NAME_STRING_test_base_chain__G \
+inline static const unsigned long int TYPE_FULL_NAME_HASH_VALUE = 244226146; \
+inline static const char* const TYPE_FULL_NAME = test_base_chain::G; \
+inline static const char* const TYPE_SHORT_NAME = G; 
+
+
+#undef GENERATE_BODY_test_base_chain__G
+#define GENERATE_BODY_test_base_chain__G \
+INHERITANCE_INFORMATION_test_base_chain__G \
+CURRENT_TYPE_ALIAS_test_base_chain__G \
+TYPE_FULLNAME_HASH_VALUE_NAME_STRING_test_base_chain__G \
+
+
+#define GENERATE_BODY_G GENERATE_BODY_test_base_chain__G
+
+
+-------------------------------------------
+
+
